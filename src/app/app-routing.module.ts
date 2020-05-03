2a6467584110d91cab0a11a29433844ae62d4dff
Charles Overbeck <charles.overbeck@gmail.com>
2020-05-02 23:00:11 -0700

    Add proxy, load comics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core"
 import { RouterModule, Routes } from "@angular/router"
+import { ComicsComponent } from "./comics/comics.component"
 import { HomeComponent } from "./home/home.component"
 import { RsoComponent } from "./rso/rso.component"
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
-  { path: 'rso', component: RsoComponent}
+  { path: 'rso', component: RsoComponent},
+  { path: 'comics', component: ComicsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { HttpClient, HttpClientModule } from "@angular/common/http"
 import { NgModule } from "@angular/core"
+import { FlexLayoutModule } from "@angular/flex-layout"
+import { MatMenuModule } from "@angular/material/menu"
 import { BrowserModule } from "@angular/platform-browser"
 import { NoopAnimationsModule } from "@angular/platform-browser/animations"
 import { MarkdownModule } from "ngx-markdown"
 
 import { AppRoutingModule } from "./app-routing.module"
 import { AppComponent } from "./app.component"
+import { ComicsComponent } from "./comics/comics.component"
 import { HomeComponent } from "./home/home.component"
 import { RsoComponent } from "./rso/rso.component"
 
@@ -13,14 +16,17 @@ import { RsoComponent } from "./rso/rso.component"
   declarations: [
     AppComponent,
     RsoComponent,
-    HomeComponent
+    HomeComponent,
+    ComicsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    MarkdownModule.forRoot({loader: HttpClient}),
+    FlexLayoutModule,
+    MarkdownModule.forRoot({ loader: HttpClient }),
     NoopAnimationsModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
