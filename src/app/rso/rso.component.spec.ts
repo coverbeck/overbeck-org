0cc73688d4d532c6f303a177519524ce0ad1d2b7
Charles Overbeck <charles.overbeck@gmail.com>
2020-05-10 11:37:35 -0700

    Fix some lint errors

--- src/app/rso/rso.component.spec.ts
+++ src/app/rso/rso.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing"
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RsoComponent } from "./rso.component"
+import { RsoComponent } from './rso.component';
 
 describe('RsoComponent', () => {
   let component: RsoComponent;
