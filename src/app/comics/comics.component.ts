93001e7d1c25bf78e55d77fdcc6cc6826a8bc34d
Charles Overbeck <charles.overbeck@gmail.com>
2020-06-14 22:40:56 -0700

    Only prompt for key if 403 error

--- src/app/comics/comics.component.ts
+++ src/app/comics/comics.component.ts
@@ -1,6 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { DialogComponent } from '../dialog/dialog.component';
+
+const APIKEY = 'apikey';
 
 @Component({
   selector: 'app-comics',
@@ -11,22 +14,28 @@ export class ComicsComponent implements OnInit {
 
   apikey: string;
 
-  constructor(private matDialog: MatDialog) { }
+  constructor(private matDialog: MatDialog, private httpClient: HttpClient) { }
 
   ngOnInit(): void {
-    const APIKEY = 'apikey';
-    this.apikey = localStorage.getItem(APIKEY) || '';
-    const config = new MatDialogConfig();
-    config.disableClose = true;
-    config.autoFocus = true;
-    config.data = {apikey: this.apikey};
-    const matDialogRef = this.matDialog.open(DialogComponent, config);
-    matDialogRef.afterClosed().subscribe(data => {
-      if (data) {
-        this.apikey = data.apikey;
-        localStorage.setItem(APIKEY, this.apikey);
+    this.apikey = sessionStorage.getItem(APIKEY) || '';
+    this.httpClient.get(`/api/comics?comic=adam&apikey=${this.apikey}`).subscribe(
+      null,
+      (error) => {
+        if (error.status === 403) {
+          const config = new MatDialogConfig();
+          config.disableClose = true;
+          config.autoFocus = true;
+          config.data = {apikey: this.apikey};
+          const matDialogRef = this.matDialog.open(DialogComponent, config);
+          matDialogRef.afterClosed().subscribe(data => {
+            if (data) {
+              this.apikey = data.apikey;
+              sessionStorage.setItem(APIKEY, this.apikey);
+            }
+          });
+        }
       }
-    });
+    );
   }
 
 }
