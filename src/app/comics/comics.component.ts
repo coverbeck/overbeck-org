6004c6003a91a07c4dde33aa01014ece739dd091
Charles Overbeck <charles.overbeck@gmail.com>
2020-08-22 11:34:23 -0700

    Changes for new backend (#6)
    * Changes for pure Scala backend
    API determines what images to fetch.
    start to handle caption, not very well.
    
    * New covidtracking endpoint

--- src/app/comics/comics.component.ts
+++ src/app/comics/comics.component.ts
@@ -5,6 +5,19 @@ import { DialogComponent } from '../dialog/dialog.component';
 
 const APIKEY = 'apikey';
 
+interface ScrapeElement {
+  id: number;
+  style: string;
+  elementType: string;
+}
+
+interface ImageData {
+  url: string;
+  style: string;
+  caption?: string;
+  id: number;
+}
+
 @Component({
   selector: 'app-comics',
   templateUrl: './comics.component.html',
@@ -13,6 +26,7 @@ const APIKEY = 'apikey';
 export class ComicsComponent implements OnInit {
 
   apikey: string;
+  images: Array<ImageData> = [];
 
   constructor(private matDialog: MatDialog, private httpClient: HttpClient) { }
 
@@ -21,14 +35,32 @@ export class ComicsComponent implements OnInit {
     if (!this.apikey) {
       this.promptForKey();
     }
-    this.httpClient.get(`/api/comics?comic=adam&apikey=${this.apikey}`).subscribe(
-      null,
+    this.httpClient.get<Array<ScrapeElement>>(`/api/portal?apikey=${this.apikey}`).subscribe(
+      (scrapeElements) => {
+        this.images = scrapeElements
+          .filter(s => s.elementType === 'image')
+          .map(s => {
+            return { url: this.urlForElement(s), style : s.style, id: s.id };
+          });
+        const textElements = scrapeElements.filter(s => s.elementType === 'text');
+        textElements.forEach(t => this.httpClient.get(this.urlForElement(t), {responseType: 'text'}).subscribe(
+          (text => {
+            // Lame; assume caption is for image with one less id number
+            this.images.find(i => i.id === (t.id - 1)).caption = text;
+          }),
+          (error => console.log(error))
+        ));
+        },
       (error) => {
         if (error.status === 403) {
           this.promptForKey();
         }
       }
     );
+  }
+
+  private urlForElement(scrapeElement: ScrapeElement) {
+    return `/api/portal/${scrapeElement.id}?apikey=${this.apikey}`;
   }
 
   private promptForKey() {
