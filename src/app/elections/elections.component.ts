e7098632c3f8e017666ae076a93c20691d3f605f
Charles Overbeck <charles.overbeck@gmail.com>
2020-12-08 22:07:06 -0800

    Table basically working

--- src/app/elections/elections.component.ts
+++ src/app/elections/elections.component.ts
@@ -2,41 +2,30 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
-
-interface HouseData {
-  year: number;
-  state: string;
-  district: number;
-  democrat: number;
-  republican: number;
-  other: number;
-  total: number;
-  winningParty: string;
-  winningVotes: number;
-  winningCandidate: string;
-}
+import { ElectionsService, HouseSummaryData, HouseSummaryDataPlus } from './elections.service';
 
 
 @Component({
   selector: 'app-elections',
   templateUrl: './elections.component.html',
-  styleUrls: ['./elections.component.scss']
+  styleUrls: ['./elections.component.scss'],
 })
 export class ElectionsComponent implements OnInit {
 
 
-  dataSource: MatTableDataSource<HouseData> = new MatTableDataSource<HouseData>();
+  dataSource: MatTableDataSource<HouseSummaryData> = new MatTableDataSource<HouseSummaryDataPlus>();
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  displayedColumns: string[] = ['year', 'state', 'district', 'winningParty', 'winningVotes'];
+  readonly displayedColumns: string[] = ['year', 'democratSeats', 'democratVotes',
+    'republicanSeats', 'republicanVotes', 'independentSeats', 'independentVotes'];
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private electionsService: ElectionsService) {
   }
 
   ngOnInit(): void {
-    this.httpClient.get<HouseData[]>('/api/elections/house')
+    this.httpClient.get<HouseSummaryData[]>('/api/elections/house?summary=true')
       .subscribe(resp => {
-        this.dataSource.data = resp;
+        this.dataSource.data = this.electionsService.extendHouseData(resp);
         this.dataSource.paginator = this.paginator;
         this.dataSource.paginator.firstPage();
       });
--- src/app/elections/elections.service.spec.ts
+++ src/app/elections/elections.service.spec.ts
@@ -2,6 +2,93 @@ import { TestBed } from '@angular/core/testing';
 
 import { ElectionsService } from './elections.service';
 
+const DATA_SNIPPET = [
+  {
+    year: 1976,
+    state: 'AK',
+    district: 0,
+    democrat: 34194,
+    republican: 83722,
+    other: 292,
+    total: 118208,
+    winningParty: 'republican',
+    winningVotes: 83722,
+    winningCandidate: 'Don Young',
+  },
+  {
+    year: 1976,
+    state: 'AL',
+    district: 1,
+    democrat: 58906,
+    republican: 98257,
+    other: 7,
+    total: 157170,
+    winningParty: 'republican',
+    winningVotes: 98257,
+    winningCandidate: 'Jack Edwards',
+  },
+  {
+    year: 1976,
+    state: 'AL',
+    district: 2,
+    democrat: 66288,
+    republican: 90069,
+    other: 5,
+    total: 156362,
+    winningParty: 'republican',
+    winningVotes: 90069,
+    winningCandidate: 'William L. \\\\Bill\\\\ Dickinson',
+  },
+  {
+    year: 1976,
+    state: 'AL',
+    district: 3,
+    democrat: 106935,
+    republican: 0,
+    other: 1113,
+    total: 108048,
+    winningParty: 'democrat',
+    winningVotes: 106935,
+    winningCandidate: 'Bill Nichols',
+  },
+  {
+    year: 2018,
+    state: 'WV',
+    district: 2,
+    democrat: 88011,
+    republican: 110504,
+    other: 6277,
+    total: 204792,
+    winningParty: 'republican',
+    winningVotes: 110504,
+    winningCandidate: 'Alex X. Mooney',
+  },
+  {
+    year: 2018,
+    state: 'WV',
+    district: 3,
+    democrat: 76340,
+    republican: 98645,
+    other: 0,
+    total: 174985,
+    winningParty: 'republican',
+    winningVotes: 98645,
+    winningCandidate: 'Carol Miller',
+  },
+  {
+    year: 2018,
+    state: 'WY',
+    district: 0,
+    democrat: 59903,
+    republican: 127963,
+    other: 13379,
+    total: 201245,
+    winningParty: 'republican',
+    winningVotes: 127963,
+    winningCandidate: 'Liz Cheney',
+  },
+];
+
 describe('ElectionsService', () => {
   let service: ElectionsService;
 
@@ -12,5 +99,10 @@ describe('ElectionsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return two years', () => {
+    const map = service.groupHouseDataByYear(DATA_SNIPPET);
+    expect(map.size).toBe(2);
   });
 });
