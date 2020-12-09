6e654796cf4fe56662a5146e2c76aa16c4ffe0b7
Charles Overbeck <charles.overbeck@gmail.com>
2020-12-08 22:43:04 -0800

    Add a graph!

--- src/app/elections/elections.component.ts
+++ src/app/elections/elections.component.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
 import { ElectionsService, HouseSummaryData, HouseSummaryDataPlus } from './elections.service';
 
 
@@ -15,6 +17,8 @@ export class ElectionsComponent implements OnInit {
 
   dataSource: MatTableDataSource<HouseSummaryData> = new MatTableDataSource<HouseSummaryDataPlus>();
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  labels: Label[];
+  graphData: ChartDataSets[];
 
   readonly displayedColumns: string[] = ['year', 'democratSeats', 'democratVotes',
     'republicanSeats', 'republicanVotes', 'independentSeats', 'independentVotes'];
@@ -25,9 +29,31 @@ export class ElectionsComponent implements OnInit {
   ngOnInit(): void {
     this.httpClient.get<HouseSummaryData[]>('/api/elections/house?summary=true')
       .subscribe(resp => {
-        this.dataSource.data = this.electionsService.extendHouseData(resp);
+        const extendHouseData = this.electionsService.extendHouseData(resp);
+        this.dataSource.data = extendHouseData;
         this.dataSource.paginator = this.paginator;
         this.dataSource.paginator.firstPage();
+        this.labels = resp.map(r => r.year + '');
+        this.graphData = [
+          {
+            data: extendHouseData.map(r => r.republicanVotesPercent),
+            label: 'Republican Vote Percentage',
+            fill: false
+          }, {
+           data: extendHouseData.map(r => r.democratVotesPercent),
+           label: 'Democrat Vote Percentage',
+           fill: false
+          },
+          {
+            data: extendHouseData.map(r => r.republicanSeatsPercent),
+            label: 'Republican Percentage Seats',
+            fill: false
+          }, {
+            data: extendHouseData.map(r => r.democratSeatsPercent),
+            label: 'Democrat Percentage Seats',
+            fill: false
+          }
+        ];
       });
   }
 
